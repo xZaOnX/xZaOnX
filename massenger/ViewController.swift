31c0696e88a7fb6ac81bb53cdfd730ea8e772e31
Ozan Öneyman <ersinozan.onayman@halkbank.com.tr>
2024-07-22 14:01:04 +0300

    Initial Commit

---
+++ massenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  massenger
+//
+//  Created by Ozan Öneyman on 22.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
